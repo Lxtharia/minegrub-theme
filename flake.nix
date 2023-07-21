52f0378942bd8bd1b233dd4e3955653903bc7693
ocfox <i@ocfox.me>
2023-07-21 22:00:42 +0800

    flake: add custom splash support

--- flake.nix
+++ flake.nix
@@ -6,41 +6,94 @@
   };
 
   outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs optional;
+      eachSystem = f: genAttrs
+        [
+          "aarch64-darwin"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ]
+        (system: f nixpkgs.legacyPackages.${system});
+
+      minegrub =
+        { pkgs, splash ? "", customSplash ? splash != "", ... }:
+        pkgs.stdenv.mkDerivation {
+          name = "minegrub-theme";
+          src = "${self}";
+
+          buildInputs = [ ] ++ optional customSplash
+            [
+              (pkgs.python3.withPackages
+                (p: [ p.pillow ]))
+            ];
+
+          patchPhase = ''
+            sed -i '$d' update_theme.py
+          '';
+
+          buildPhase = optional customSplash ''
+            echo "${splash}" > resources/splashes.txt
+            python update_theme.py
+          '';
+
+          installPhase = ''
+            mkdir -p $out/grub/themes/minegrub
+            cp *.png $out/grub/themes/minegrub
+            cp *.pf2 $out/grub/themes/minegrub
+            cp theme.txt $out/grub/themes/minegrub
+          '';
+        };
+    in
     {
       nixosModules.default = { config, pkgs, ... }:
         let
           cfg = config.boot.loader.grub.minegrub-theme;
-          lib = nixpkgs.lib;
-          minegrub-theme = pkgs.stdenv.mkDerivation {
-            name = "minegrub-theme";
-            src = "${self}";
-            installPhase = ''
-              mkdir -p $out/grub/themes/minegrub
-              cp *.png $out/grub/themes/minegrub
-              cp *.pf2 $out/grub/themes/minegrub
-              cp theme.txt $out/grub/themes/minegrub
-            '';
-          };
+          inherit (nixpkgs.lib) mkOption types mkIf;
         in
         {
           options = {
             boot.loader.grub.minegrub-theme = {
-              enable = lib.mkOption {
+              splash = mkOption {
+                default = "deleting garbage...";
+                example = "Infinite recursion";
+                type = types.str;
+                description = ''
+                  Splash text on logo.
+                '';
+              };
+              enable = mkOption {
                 default = false;
                 example = true;
-                type = lib.types.bool;
+                type = types.bool;
                 description = ''
-                  Enable minegrub theme
+                  Enable minegrub theme.
                 '';
               };
             };
           };
-          config = lib.mkIf cfg.enable {
-            boot.loader.grub = {
-              theme = "${minegrub-theme}/grub/themes/minegrub";
-              splashImage = "${minegrub-theme}/grub/themes/minegrub/background.png";
-            };
+          config = mkIf cfg.enable {
+            boot.loader.grub =
+              let
+                minegrub-theme = minegrub {
+                  inherit pkgs;
+                  splash = cfg.splash;
+                };
+              in
+              {
+                theme = "${minegrub-theme}/grub/themes/minegrub";
+                splashImage = "${minegrub-theme}/grub/themes/minegrub/background.png";
+              };
           };
         };
+
+      packages = eachSystem
+        (pkgs: {
+          default = minegrub {
+            inherit pkgs;
+            # splash = "custom splash text";
+          };
+        });
     };
 }
