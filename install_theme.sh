d0aec4523e1800c3196227dc420d326891292508
Lxtharia <maud-lin@gmx.de>
2026-07-05 02:05:25 +0200

    Remove -n 1 flags from read command

--- install_theme.sh
+++ install_theme.sh
@@ -25,7 +25,7 @@ theme_path="$grub_path/themes/minegrub"
 ## Prompts
 
 # Choosing a background, comment this out if it's annoying
-read -p "[?] Do you want to choose a specific background? [y/N] " -en 1 choose_bg 
+read -p "[?] Do you want to choose a specific background? [y/N] " -e choose_bg 
 if [[ "$choose_bg" =~ y|Y ]]; then
     echo "[INFO] Choosing a background from ./background_options/"
     "$SCRIPT_DIR/choose_background.sh"
@@ -34,7 +34,7 @@ else
 fi
 
 echo
-read -p "[?] Copy/Update the theme to '$theme_path'? [Y/n] " -en 1 copy_theme
+read -p "[?] Copy/Update the theme to '$theme_path'? [Y/n] " -e copy_theme
 if [[ "$copy_theme" =~ y|Y || -z "$copy_theme" ]]; then
     echo "[INFO] => Copying the theme files to boot partition:"
     # copy recursive, update, verbose
@@ -62,7 +62,7 @@ fi
 case "$INIT_SYS" in
     systemd)
         echo
-        read -p "[?] Do you want to install a systemd service to automatically update the splash texts and backgrounds after every boot? [y/N] " -en 1 skip_service_installation
+        read -p "[?] Do you want to install a systemd service to automatically update the splash texts and backgrounds after every boot? [y/N] " -e skip_service_installation
         if [[ "$skip_service_installation" =~ y|Y ]]; then
             echo -ne "[INFO] Installing systemd service to update splash and package labels on boot\n\t"
             cp -uv "$SCRIPT_DIR/minegrub-update.service" /etc/systemd/system/
@@ -72,7 +72,7 @@ case "$INIT_SYS" in
     ;;
     openrc)
         echo
-        read -p "[?] Do you want to install an OpenRC service to automatically update the splash texts and backgrounds after every boot? [y/N] " -en 1 skip_openrc_service_installation
+        read -p "[?] Do you want to install an OpenRC service to automatically update the splash texts and backgrounds after every boot? [y/N] " -e skip_openrc_service_installation
         if [[ "$skip_openrc_service_installation" =~ y|Y ]]; then
             echo -ne "[INFO] Installing openrc service to update splash and package labels on boot\n\t"
             cp -uv "$SCRIPT_DIR/minegrub-update.openrc" /etc/init.d/minegrub-update
@@ -87,7 +87,7 @@ case "$INIT_SYS" in
 esac
 
 echo
-read -p "[?] Do you want a grub drop-in-config file to be edited so setting GRUB_BACKGROUND will set a background for the grub console? [y/N] " -en 1 skip_patch
+read -p "[?] Do you want a grub drop-in-config file to be edited so setting GRUB_BACKGROUND will set a background for the grub console? [y/N] " -e skip_patch
 if [[ "$skip_patch" =~ y|Y ]]; then
     echo "[INFO] Editing /etc/grub.d/00_header"
     # Backing up that file, just in case
